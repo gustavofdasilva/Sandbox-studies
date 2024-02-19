b383ae7910a09fb858b24749fbf64ea9c96c8e06
Gustavo Ferreira da Silva <gustavofsilvas@gmail.com>
2024-02-19 15:54:57 -0300

    mais interação com excel

--- python/DataProcessing/pandasInteracting.py
+++ python/DataProcessing/pandasInteracting.py
@@ -15,14 +15,33 @@
 
 # writer.close()
 
-import pandas as pd
-import json
+# import pandas as pd
+# import json
 
-with open('C:/Gustavo/repos/Sandbox-studies/python/DataProcessing/content/projectsinfo.json') as file:
-    data = json.load(file)
-    writer = pd.ExcelWriter('C:/Gustavo/repos/Sandbox-studies/python/DataProcessing/content/toJson.xlsx',engine='xlsxwriter', if_sheet_exists=None, mode='w')
-    df = pd.DataFrame(data)
-    df.to_excel(writer,sheet_name='toJson',index=False)
-    writer.close()
+# with open('C:/Gustavo/repos/Sandbox-studies/python/DataProcessing/content/projectsinfo.json') as file:
+#     data = json.load(file)
+#     writer = pd.ExcelWriter('C:/Gustavo/repos/Sandbox-studies/python/DataProcessing/content/toJson.xlsx',engine='xlsxwriter', if_sheet_exists=None, mode='w')
+#     df = pd.DataFrame(data)
+#     df.to_excel(writer,sheet_name='toJson',index=False)
+#     writer.close()
     
-    
+# import pandas as pd
+# excel = pd.ExcelFile('C:/Gustavo/repos/Sandbox-studies/python/DataProcessing/content/empty_test3.xlsx')
+# df = pd.read_excel(excel)['Name'][1]
+# print(df)
+
+import pandas as pd
+writer = pd.ExcelWriter('C:/Gustavo/repos/Sandbox-studies/python/DataProcessing/content/empty_test3.xlsx', mode='a', engine='openpyxl', if_sheet_exists='replace')
+data=[
+    {'Name':'Hugo',"Age":'23'},
+    {'Name':'ugo',"Age":'25'},
+    {'Name':'go',"Age":'27'}
+    ]
+
+df = pd.read_excel('C:/Gustavo/repos/Sandbox-studies/python/DataProcessing/content/empty_test3.xlsx', sheet_name='1')
+newRow = {'Name':'go',"Age":'27'}
+df['Age']=df['Age']*2
+
+df.to_excel(writer,sheet_name='1',index=False)
+
+writer.close()
