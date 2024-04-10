b58eff5e32cd68de0b544c99ab5d95b16e19def6
Gustavo Ferreira da Silva <gustavofsilvas@gmail.com>
2024-04-10 12:53:59 -0300

    continuando protótipo webscraping

--- node/webScraping/index.js
+++ node/webScraping/index.js
@@ -3,8 +3,14 @@ import jQuery from "jquery";
 import jsdom from 'jsdom'
 
 const getQuotes = async () => {
+    //? Parametros que serão passados
+    // Carregar qual página? page index
+    const offset = 12 // quinto andar carrega de 12 em 12 anúncios
+
+    //?
+
     const browser = await puppeteer.launch({
-        headless: true,
+        headless: false,
         defaultViewport: null,
     })
 
@@ -14,30 +20,39 @@ const getQuotes = async () => {
         waitUntil: "domcontentloaded",
     })
 
+    const pageIndex = 20
+    
+    let element = await page.waitForSelector('div.MuiBox-root.mui-10smzr0')
+    for(let i = 0; i<pageIndex;i++) {
+        await element.click()
+        await page.waitForNavigation()
+        element = await page.waitForSelector('div.MuiBox-root.mui-10smzr0')
+    }
     
 
     const pageData = await page.evaluate(()=>{
-        const allRents = {}
+        const pageIndex = 20
         const rentCardNodes = document.querySelectorAll('.bTndEd')
-        const rentCardArr = [...rentCardNodes].map((e)=>{
+        const rentCardSelected = [...rentCardNodes].slice(pageIndex*12,(pageIndex*12)+12)
+        const rentCardArr = [...rentCardSelected].map((e)=>{
             const imgs = [...e.querySelectorAll('img')].map((e)=>{
                 return ('https://www.quintoandar.com.br'+e.getAttribute('src')).replace('med','xxl')
             })
             return {
                 title: e.firstChild.getAttribute('title'),
-                link: e.firstChild.getAttribute('href'),
-                rentPrice: {
-                    text: e.querySelector('.fLkTfH').innerText,
-                    rawPrice: (e.querySelector('.fLkTfH').innerText).replace(/\D+/g, '')
-                },
-                totalPrice: {
-                    text: e.querySelector('.gYqoUK').innerText,
-                    rawPrice: (e.querySelector('.gYqoUK').innerText).replace(/\D+/g, '')
-                },
-                address: e.querySelector('.hGkWsg').innerText,
-                type: e.querySelector('.jxBqSr').innerText,
-                basicInfo: e.querySelector('.hZkulO').innerText,
-                imgs,
+                // link: e.firstChild.getAttribute('href'),
+                // rentPrice: {
+                //     text: e.querySelector('.fLkTfH').innerText,
+                //     rawPrice: (e.querySelector('.fLkTfH').innerText).replace(/\D+/g, '')
+                // },
+                // totalPrice: {
+                //     text: e.querySelector('.gYqoUK').innerText,
+                //     rawPrice: (e.querySelector('.gYqoUK').innerText).replace(/\D+/g, '')
+                // },
+                // // address: e.querySelector('.hGkWsg').innerText,
+                // // type: e.querySelector('.jxBqSr').innerText,
+                // basicInfo: e.querySelector('.hZkulO').innerText, //! Fazer split no "·" para separa cada info
+                // imgs,
 
             }
         })
@@ -72,7 +87,8 @@ const getQuotes = async () => {
 
     console.log(pageData)
 
-    await browser.close()
+    await page.close()
+    
 
 }
 
